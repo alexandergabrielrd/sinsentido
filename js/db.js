c09bec334f4d0872edf73f95e1039029c9059814
alexandergabrielrd <agrd2002@gmail.com>
2017-02-18 23:53:02 -0500

    Data base

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 ﻿QandAObj = { "qa":[{
- 	"list": [{
+ 	"list": [{ //listassss corona
  		"q": "¿Cuál es el órgano más fuerte del cuerpo humano?",
  		"o": ["Glúteos", "Lengua", "Abdomen", "Ninguna de las anteriores"],
  		"a": "2"
@@ -41,214 +41,214 @@
  		"a": "2"
  	},]
  }, {
- 	"list": [{
+ 	"list": [{ //faltan 2 gusto
  		"q": "Pregunta gusto 1",
  		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta gusto 2",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "¿Cuántos sabores pueden distinguir los quimiorreceptores de la lengua?",
+ 		"o": ["6", "5", "7", "3"],
+ 		"a": "2"
+ 	}, {
+ 		"q": "¿En qué procesos interviene la lengua?",
+ 		"o": ["Masticación", "Habla", "Todas las anteriores", "Ninguna de las anteriores"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta gusto 3",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "Sentimos calor en la lengua gracias a...",
+ 		"o": ["Papilas filiformes", "Papilas fungiformes", "Papilas caliciformes", "Todas las anteriores"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta gusto 4",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "¿Porque la lengua tiene apariencia rugosa? Gracias a...",
+ 		"o": ["Las papilas gustativas", "El roce con el paladar", "Todas las anteriores", "Ninguna de las anteriores"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta gusto 5",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Cuál tipo de papila gustativa se encargan de percibir el sabor salado?",
+ 		"o": ["Filiforme", "Fungiforme", "Caliciforme", "Ninguna de las anteriores"],
+ 		"a": "2"
  	}, {
- 		"q": "Pregunta gusto 6",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta gusto 7",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Dónde se encuentran los quimiorreceptores de la lengua?",
+ 		"o": ["Papilas gustativas", "Saliva", "Todas las anteriores", "Ninguna de las anteriores"],
+ 		"a": "4"
  	}, {
  		"q": "Pregunta gusto 8",
  		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta gusto 9",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "¿Qué nervios intervienen en el gusto?",
+ 		"o": ["VII y V", "IX, VII y X", "Todos los anteriores", "II y IX"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta gusto 10",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "¿Cuál de estas afirmacioes es correcta?",
+ 		"o": ["El sabor 'umami' fue descubrierto por un japonés", "La papila filiforme capta lo salado", "Las quimiorreceptores se encuentran en los botones gustativo", "Ninguna es correcta"],
  		"a": "1"
  	}]
  }, {
- 	"list": [{
+ 	"list": [{//faltan 3 oido
  		"q": "Pregunta oido 1",
  		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta oido 2",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "¿Cuáles son las clases de equilibrio del organismo humano?",
+ 		"o": ["Espacial y automático", "Dinámico y espacial", "Estático y dinámico", "Ninguna de las anteriores"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta oido 3",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Qué tipo de receptores se necargan de la audición?",
+ 		"o": ["Mecanoceptor", "Fotorreceptor", "Nociceptor", "Ninguna de las anteriores"],
+ 		"a": "4"
  	}, {
  		"q": "Pregunta oido 4",
  		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta oido 5",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "¿Cuáles son las partes del laberinto ósea?",
+ 		"o": ["Oido interno y medio", "Martillo, yunque y estribo", "Vestíbulo, cóclea y conductos semicirculares", "Ninguno de los anteriores"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta oido 6",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Qué célula reacciona a la gravedad y movimiento?",
+ 		"o": ["Celulas timpánica", "Célula ciliar", "Célula oval", "Ninguna de las anteriores"],
+ 		"a": "2"
  	}, {
- 		"q": "Pregunta oido 7",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Cómo se llama el líquido donde se encuntran los mecanorreceptores?",
+ 		"o": ["Líquido sáculo", "Líquido linfa", "Líquido timpánico", "Ninguna de las anteriores"],
+ 		"a": "2"
  	}, {
- 		"q": "Pregunta oido 8",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Cuándo unafrecuencia se vuelve ultrasonido? ",
+ 		"o": ["Mayor a 20000 Hz", "Mayor a 40000 Hz", "Mayor de 15000 Hz", "Ninguna de las anteriores"],
+ 		"a": "2"
  	}, {
  		"q": "Pregunta oido 9",
  		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta oido 10",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "¿Cuál de estas afirmaciones es correcta?",
+ 		"o": ["El timpano se divide en oido interno y externo", "El oido informa acerca de la posición del cuerpo en el espacio", "La oreja esta formada por hueso blando", "Ninguna es correcta"],
+ 		"a": "2" 
+ 	}]
+ }, {
+ 	"list": [{//listasss olfato
+ 		"q": "¿Qué tipo de receptores se encuentran en la nariz?",
+ 		"o": ["Mecanorreceptores", "Quimioceptores", "Nociceptores", "Ninguna de las anteriores"],
+ 		"a": "4"
+ 	}, {
+ 		"q": "¿Cómo se llama el hueso que separa el bulbo olfatorio de la cavidad nasal?",
+ 		"o": ["Cretoides", "Zetoides", "Etmoides", "Zeptoides"],
+ 		"a": "3"
+ 	}, {
+ 		"q": "¿Cuáles son las células asociadas de las olfatorias?",
+ 		"o": ["Glandulares y ciliares", "Ciliares y sustentaculares", "Glandulares y sustentaculares", "Todas las anteriores"],
+ 		"a": "1"
+ 	},{
+ 		"q": "¿En qué parte de la cavidad nasal se calienta el aire?",
+ 		"o": ["Fosa nasal", "Bulbo olfatorio", "Pituitaria roja", "Ninguna de las anteriores"],
+ 		"a": "1"
+ 	}, {
+ 		"q": "¿Dónde se ubica el bulbo olfatorio?",
+ 		"o": ["Lóbulo occipital", "Lóbulo frontal", "Lóbulo temporal", "Ninguna de las anteriores"],
+ 		"a": "2"
+ 	}, {
+ 		"q": "¿Qué promedio de vida tiene una célula olfatoria?",
+ 		"o": ["3 meses", "15 días", "1 mes", "45 días"],
+ 		"a": "3"
+ 	}, {
+ 		"q": "¿Dónde se localizan las células olfatorias?",
+ 		"o": ["Fosa nasal", "Pituitaria roja", "Pituitaria amarilla", "Todas las anteriores"],
+ 		"a": "3"
+ 	}, {
+ 		"q": "¿Qué funciones tienen las células mitrales?",
+ 		"o": ["Transmiten info. a la corteza cerebral", "Receptores accesorias", "Todas las anteriores", "Ninguna de las anteriores"],
+ 		"a": "1"
+ 	}, {
+ 		"q": "¿Qué pasa cuando se olfatea el mismo olor durante un tiempo determinado?",
+ 		"o": ["Se mantiene igual", "Se realza", "Se disminuye", "El olor cambia"],
+ 		"a": "3"
+ 	},  {
+ 		"q": "¿Cuál de estas afirmaciones es correcta?",
+ 		"o": ["La célula olfatoria es una neurona bipolar", "Los cilios entran en contacto con el moco ", "Todas las anteriores", "Ninguna de las anteriores"],
+ 		"a": "3"
+ 	}]
+ }, {
+ 	"list": [{ //listassss tacto
+ 		"q": "Capa más profunda de la piel ...",
+ 		"o": ["Cornea", "Epidermis", "Dermis", "Tejido Subcutaneo"],
+ 		"a": "3"
+ 	}, {
+ 		"q": "Corpúsculo de la piel que detecta el frio",
+ 		"o": ["Pacini", "Krause", "Ruffini", "Meissner"],
+ 		"a": "2"
+ 	}, {
+ 		"q": "¿Cuál de las opciones es una estructura accesoria de la piel?",
+ 		"o": ["Glándulas hipofisiarias", "Glándula sebacea", "Todas las anteriores", "Ninguna de las anteriores"],
+ 		"a": "2"
+ 	}, { 
+ 		"q": "La uña esta compuesta por...",
+ 		"o": ["Células muertas", "Células óseas", "Células sobrantes", "Ninguna de las anteriores"],
+ 		"a": "1"
+ 	}, {
+ 		"q": "¿Cuál de estas no es una célula especializada?",
+ 		"o": ["Corpusculos de krause", "Terminaciones libres", "Corpusculos de Meissner", "Ninguna de las anteriores"],
+ 		"a": "2"
+ 	}, {
+ 		"q": "¿Qué función tiene la melanina en la piel?",
+ 		"o": ["Pigmentación", "Elasticidad", "Renovación", "Control de temperatura"],
+ 		"a": "1"
+ 	}, {
+ 		"q": "Cavidad interna que ocupa la raíz de un pelo...",
+ 		"o": ["Glándula sebacea", "Poros de la piel", "Folículo piloso", "Ninguna de las anteriores"],
+ 		"a": "1"
+ 	}, {
+ 		"q": "Las sensaciones de texturas son percibidas por...",
+ 		"o": ["Termorreceptores", "Mecanorreceptores", "Termoceptores", "Quimioceptores"],
+ 		"a": "2"
+ 	}, {
+ 		"q": "Los nociceptores se relacionan con...",
+ 		"o": ["Presión", "Dolor", "Todas las anteriores", "Ninguna de las anteriores"],
+ 		"a": "2"
+ 	}, {
+ 		"q": "¿Qué capa de la piel se encuentra cerca de los vasos sanguíneos?",
+ 		"o": ["Epidermis", "Bidermis", "Dermis", "Ninguna de las anteriores"],
  		"a": "1"
  	}]
  }, {
- 	"list": [{
- 		"q": "Pregunta olfato 1",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 	"list": [{ //listasss vision
+ 		"q": "La visión existe gracias a...",
+ 		"o": ["La luz", "El calor", "Que captamos imágenes", "Todas las anteriores"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta olfato 2",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
+ 		"q": "Parte del ojo que posee bastones y conos...",
+ 		"o": ["Esclerótica", "Coroides", "Cristalino", "Retina"],
+ 		"a": "4"
+ 	}, {
+ 		"q": "Recibo estímulos luminosos y me encuentro en la retina ¿Quién soy?",
+ 		"o": ["Interoceptor", "Fotoceptor", "Nervio Optico", "Ninguna de las anteriores"],
+ 		"a": "4"
+ 	}, {
+ 		"q": "¿Cuántos tipos de receptores existen en el ojo?",
+ 		"o": ["3", "1", "2", "6"],
+ 		"a": "3"
+ 	}, {
+ 		"q": "¿Qué parte del ojo se contrae con la presencia de mucha luz?",
+ 		"o": ["Pupila", "Cristalino", "Párpados", "Iris"],
+ 		"a": "4"
+ 	}, {
+ 		"q": "¿Que defecto ocasiona que la imagen se forma delante de la retina?",
+ 		"o": ["Miopía", "Astigmatismo", "Presbicia", "Ninguna de las anteriores"],
  		"a": "1"
  	}, {
- 		"q": "Pregunta olfato 3",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Cuál de estas afirmaciones es correcta?",
+ 		"o": ["El ojo capta imágenes", "El iris es un tabique", "La retina es la capa mas externa del ojo", "Ninguna es correcta"],
+ 		"a": "2"
  	}, {
- 		"q": "Pregunta olfato 4",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "¿Quién se encarga de lograr la visión binocular?",
+ 		"o": ["Cristalino", "Músculos Intrínsecos", "El iris", "Ninguna de las anteriores"],
+ 		"a": "4"
  	}, {
- 		"q": "Pregunta olfato 5",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "Liquido con consistencia gelatinosa que mantiene al ojo con una forma redonda...",
+ 		"o": ["Humor acuoso", "Cristalino", "Liquido óptico-neuronal", "Humor Vitreo"],
+ 		"a": "4"
  	}, {
- 		"q": "Pregunta olfato 6",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta olfato 7",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta olfato 8",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta olfato 9",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta olfato 10",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}]
- }, {
- 	"list": [{
- 		"q": "Pregunta tacto 1",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 2",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 3",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 4",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 5",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 6",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 7",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 8",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 9",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta tacto 10",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}]
- }, {
- 	"list": [{
- 		"q": "Pregunta vista 1",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 2",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 3",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 4",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 5",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 6",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 7",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 8",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 9",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
- 	}, {
- 		"q": "Pregunta vista 10",
- 		"o": ["Fiesta", "Focus", "Mustang", "Uno"],
- 		"a": "1"
+ 		"q": "la única parte del interior del ojo que no tiene retina",
+ 		"o": ["Coroides", "Todo su interior tiene retina", "Punto ciego", "Ninguna de las anteriores"],
+ 		"a": "3"
  	}]
  }]}
 
